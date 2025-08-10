1936f00b7182348668b225c7b0a7658853fec3ef
Brian Zheng <zhengbrian1@gmail.com>
2025-08-09 22:11:05 -0400

    manually initialize weights according to openai's gpt2 model

--- train_gpt2.py
+++ train_gpt2.py
@@ -12,6 +12,7 @@ class CasualSelfAttention(nn.Module):
         # k,q,v projections for all heads
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.SCALE_INIT = 1.0
 
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -49,6 +50,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.SCALE_INIT = 1.0 
 
     def forward(self, x):
         x = self.c_fc(x) # linear expansion
@@ -102,6 +104,22 @@ class GPT(nn.Module):
         # we also expect the wte to react similarly for synonyms, and the lm_head to give synonyms similar scores
         # for more information, see https://arxiv.org/pdf/1608.05859 
         self.transformer.wte.weight = self.lm_head.weight # also saves a lot of parameters
+
+        # init params
+        self.apply(self._init_weights) # apply to every submodule
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            # scale down weights of c_proj in mlp and attn
+            if hasattr(module, "SCALE_INIT"):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 
     def forward(self, idx, targets=None):
         # idx: (B, T)
