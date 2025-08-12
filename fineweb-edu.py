4bee0b2006ea5b09f1ccfd5bfddbc924063fb270
Brian Zheng <zhengbrian1@gmail.com>
2025-08-12 19:33:57 -0400

    -  Did not commit for a while; I don't know why I thought it would be a good idea to try to push 20GB of data files to github
    -  Write fineweb-edu.py to download and tokenize the data from the FineWeb-Edu 10B token subset, and then save the data in shards to a local directory.
    
    - Update the DataLoaderLite for validation split, and in the training loop periodically use the validation data to calculate val loss
    
    - Also periodically sample text generations in the training loop using old code
    
    -  add checkpoint saving every 5000 steps
    
    -  Write goldenswag.py to download the goldenswag data examples, render examples for evaluation, and evaluate pretrained models
    
    -  Periodically evaluate using goldenswag evaluation in the training loop
    
    -  Log the loss, goldenswag accuracy, and validation loss
    
    -  Simply copy pasted the code for graphing these metrics, will come back to internalize the code later

---
+++ fineweb-edu.py
@@ -0,0 +1,92 @@
+"""
+Uses 10B token subset of FineWeb-Edu dataset
+https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu
+Downloads and tokenizes the data, and then saves the data shards to the "edu_fineweb10B" local directory
+"""
+
+import os
+import multiprocessing as mp # running multiple processes in parallel
+import numpy as np
+import tiktoken
+from datasets import load_dataset
+from tqdm import tqdm 
+
+local_dir = "fineweb-edu-10B"
+
+shard_size = int(1e8) # 100M tokens per shard, 100 total shards
+
+# create the local dir for the data
+DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir) # __file__ is the path to this file, dirname gets the folder
+os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+# fw_edu = load_dataset("HuggingFaceFW/fineweb-edu", name="sample-10BT", split="train")
+fw_edu = load_dataset("HuggingFaceFW/fineweb-edu", name="sample-10BT", split="train")
+
+
+enc = tiktoken.get_encoding("gpt2")
+eot = enc._special_tokens["<|endoftext|>"]
+
+def tokenize(doc):
+    """tokenizes a single document from the dataset and returns a numpy array of uint16 tokens"""
+    tokens = [eot] # end of text token seperates documents from each other
+    tokens.extend(enc.encode_ordinary(doc["text"])) # ignore special tokens (like eot) in the dataset
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "document token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16) # save memory
+    return tokens_np_uint16
+
+def write_datafile(filename, tokens_np):
+    """saves tokens numpy array to a binary file in .npy format"""
+    np.save(filename, tokens_np)
+
+
+if __name__ == "__main__":
+    # tokenize all documents and write output shards
+    num_processes = max(1, os.cpu_count() // 2)
+    with mp.Pool(num_processes) as pool: # make pool with num_processes worker processes
+        shard_idx = 0
+        shard_tokens_np = np.empty((shard_size, ), dtype=np.uint16)
+        shard_token_count = 0
+        progress_bar = None
+        
+        # maps the tokenize function onto all items (documents) in fw_edu
+        for tokens in pool.imap(tokenize, fw_edu, chunksize=16): # disbatch chunks of 16 at a time to each worker
+
+            if shard_token_count + len(tokens) <= shard_size:
+                shard_tokens_np[shard_token_count : shard_token_count + len(tokens)] = tokens
+                shard_token_count += len(tokens)
+                if progress_bar is None:
+                    progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_idx}")
+                    progress_bar.update(shard_token_count) # add in leftover tokens
+                else:
+                    progress_bar.update(len(tokens))
+
+            else:
+                # write the current shard to a file and start a new shard
+                split = "val" if shard_idx == 0 else "train"
+                filename = os.path.join(DATA_CACHE_DIR, f"fineweb-edu_{split}_{shard_idx:06d}")
+
+                # put whatever fits from the document into this shard, and put rest in next shard
+                remainder = shard_size - shard_token_count
+                progress_bar.update(remainder)
+
+                shard_tokens_np[shard_token_count : shard_token_count + remainder] = tokens[:remainder]
+                write_datafile(filename, shard_tokens_np)
+
+                shard_idx += 1
+                progress_bar = None
+
+                # put the leftovers (len(tokens) - remainder) of the current document into the next shard
+                leftover_tokens = len(tokens) - remainder
+
+                shard_tokens_np[0 : leftover_tokens] = tokens[remainder:]
+                shard_token_count = leftover_tokens
+
+
+        # write any remaining tokens as last shard
+        if shard_token_count != 0:
+            split = "val" if shard_idx == 0 else "train"
+            filename = os.path.join(DATA_CACHE_DIR, f"fineweb-edu_{split}_{shard_idx:06d}")
+            write_datafile(filename, shard_tokens_np[:shard_token_count])
+
+
---
+++ goldenswag.py
@@ -0,0 +1,126 @@
+"""
+Loads and evaluates GoldenSwag
+GoldenSwag is a subset of HellaSwag that was heavily filtered in hopes
+of ensuring that the benchmark genuinely requires commonsense reasoning.
+For more information: https://arxiv.org/pdf/2504.07825
+
+The validation set has a total of 1525 examples, or ~15.2% of the original dataset.
+"""
+
+import os
+from datasets import load_dataset
+import tiktoken
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from transformers import GPT2LMHeadModel
+
+enc = tiktoken.get_encoding("gpt2")
+
+def render_example(example):
+    """
+    Render a GoldenSwag multiple-choice example into tensors for model evaluation.
+
+    Args:
+        example (dict): A single GoldenSwag example with keys:
+            - "ctx": A string containing the shared context.
+            - "endings": A list of four string completions that serve as the choices.
+            - "label": An integer (0-3) indicating the index of the correct ending.
+
+    Returns:
+        tokens: A tensor with shape (4, N) containing the tokens of the context + each ending.
+        mask: A tensor the same shape as tokens with values
+            - 0 for context tokens
+            - 1 for ending tokens (where we will evaluate probabilities).
+        label: The index of the correct ending as provided in the example.
+    """
+
+    ctx = example["ctx"]
+    endings = example["endings"]
+    label = example["label"]
+
+    ctx_tokens = enc.encode(ctx)
+    tok_rows = []
+    mask_rows = []
+
+    for ending in endings:
+        end_tokens = enc.encode(" " + ending)  # GPT-2 tokenizer needs prepending " "
+        tok_rows.append(ctx_tokens + end_tokens)
+        mask_rows.append([0]*len(ctx_tokens) + [1]*len(end_tokens)) # 0 for context tokens, 1 for ending tokens
+    
+    # get the maximum row length and make it the sequence length (T) 
+    max_len = max(len(row) for row in tok_rows)
+    tokens = torch.zeros((4, max_len), dtype=torch.long)
+    mask = torch.zeros((4, max_len), dtype=torch.long)
+
+    for i, (tok_row, mask_row) in enumerate(zip(tok_rows, mask_rows)):
+        tokens[i, :len(tok_row)] = torch.tensor(tok_row)
+        mask[i, :len(mask_row)] = torch.tensor(mask_row)
+
+    return tokens, mask, label
+
+
+@torch.inference_mode()
+def evaluate(model_type, device):
+    torch.set_float32_matmul_precision("high") # tf32
+    model = GPT2LMHeadModel.from_pretrained(model_type).to(device)
+    model = torch.compile(model)
+
+
+    num_corerct = 0
+    num_examples = 0
+
+    gs = load_dataset("PleIAs/GoldenSwag", split="validation")
+    for example in gs:
+        tokens, mask, label = render_example(example)
+        tokens = tokens.to(device)
+        mask = mask.to(device)
+
+        logits = model(tokens).logits
+
+        shift_logits = (logits[:, :-1, :]).contiguous() # we don't want the logits for the next token after the ending
+        shift_tokens = (tokens[:, 1:]).contiguous() # get all the tokens except for the first; the next tokens
+
+        # flatten to calculate CE loss
+        flat_shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+        flat_shift_tokens = shift_logits.view(-1)
+
+        shift_losses = F.cross_entropy(flat_shift_logits, flat_shift_tokens, reduction="none") # default reduction is "mean", but we want a tensor of losses for each token
+
+        shift_losses = shift_losses.view(tokens.size(0), -1)
+
+        shift_mask = (mask[:, 1:]).contiguous() 
+        masked_shift_losses = shift_losses * shift_mask # only want losses of ending tokens
+
+        sum_loss = masked_shift_losses.sum(dim=1)
+        avg_loss = sum_loss / shift_mask.sum(dim=1) # avg loss for each completion
+
+        pred = avg_loss.argmin().item()
+
+        num_examples += 1
+        num_correct += int(pred == label)
+
+        print(f"Evaluated {num_examples} examples | acc: {num_correct}/{num_examples}={num_correct/num_examples:.4f}")
+
+        if num_examples < 10:
+            print("---")
+            print(f"Context:\n {example['ctx']}")
+            print(f"Endings:")
+            for i, end in enumerate(example["endings"]):
+                print(f"{i} (loss: {avg_loss[i].item():.4f}) | {end}")
+            print(f"predicted: {pred}, actual: {label}")
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_type", type=str, default="gpt2", help="the model type to use")
+    parser.add_argument("-d", "--device", type=str, default="cuda", help="the device to use")
+    args = parser.parse_args()
+    evaluate(args.model_type, args.device)
+
+
+    
+
+
+
